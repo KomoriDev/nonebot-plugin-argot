aad8fd168775102148a4ef204dc869f4db502037
KomoriDev <mute231010@gmail.com>
2025-03-09 14:51:36 +0800

    :memo: 添加示例

--- example/plugins/demo.py
+++ example/plugins/demo.py
@@ -7,11 +7,10 @@ require("nonebot_plugin_argot")
 require("nonebot_plugin_alconna")
 
 from nonebot_plugin_alconna import Command
-from nonebot_plugin_alconna.uniseg import UniMessage
+from nonebot_plugin_alconna.uniseg import Text, Image, UniMessage
 
 from nonebot_plugin_argot import add_argot
-
-from .utils import image_to_base64
+from nonebot_plugin_argot.segment import Argot
 
 cmd1 = Command("test1").build(use_cmd_start=True)
 
@@ -19,22 +18,13 @@ cmd1 = Command("test1").build(use_cmd_start=True)
 @cmd1.handle()
 async def _():
 
-    message = await UniMessage.text("This is a text message. Reply /background to get background image.").send(
+    await UniMessage.text("This is a text message. Reply /background to get background image.").finish(
         argot={
             "name": "background",
-            "command": "background",
-            "content": "https://koishi.chat/logo.png",
+            "segment": Image(url="https://koishi.chat/logo.png"),
             "expire": 240,  # The argot will expired after 4 minutes.
         }
     )
-
-    await add_argot(
-        "author",
-        message.msg_ids[0]["message_id"],
-        content="某内鬼",
-        expire_time=timedelta(minutes=2),  # use timedelta to set expire time
-    )
-    await cmd1.finish()
 
 
 cmd2 = on_command("test2")
@@ -42,12 +32,12 @@ cmd2 = on_command("test2")
 
 @cmd2.handle()
 async def _():
-    message = await cmd2.send("A text message.")
+    message = await cmd2.send("This is a text message. Reply /background to get background image.")
     await add_argot(
-        "author",
-        message["message_id"],
-        content="某内鬼",
-        expire_time=timedelta(minutes=2),
+        message_id=message["message_id"],
+        name="background",
+        segment=Image(url="https://koishi.chat/logo.png"),
+        expired_at=timedelta(minutes=2),
     )
 
 
@@ -59,19 +49,9 @@ async def _():
 
     path: Path = Path(__file__).parent / "image.png"
 
-    message = await UniMessage.text("This is a text message. Reply /image to get image.").send(
-        argot={
-            "name": "image",
-            "command": "image",
-            "content": image_to_base64(path),
-            "expire": 240,  # The argot will expired after 4 minutes.
-        }
-    )
-
-    await add_argot(
-        "author",
-        message.msg_ids[0]["message_id"],
-        content="某内鬼",
-        expire_time=timedelta(minutes=2),  # use timedelta to set expire time
-    )
-    await cmd1.finish()
+    await UniMessage(
+        [
+            Text("This is a text message. Reply /image to get image."),
+            Argot("image", [Text("image"), Image(path=path)]),
+        ]
+    ).send()
