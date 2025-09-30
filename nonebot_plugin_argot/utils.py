4e94fce2459f75dd684157bb7f35b1d84b7e1327
KomoriDev <mute231010@gmail.com>
2025-09-30 22:59:55 +0800

    :bug: 处理 get_message_id 函数中的 Alconna Receipt 对象

--- nonebot_plugin_argot/utils.py
+++ nonebot_plugin_argot/utils.py
@@ -1,6 +1,8 @@
 from typing import Any
 from datetime import datetime, timedelta
 
+from nonebot_plugin_alconna.uniseg import Receipt
+
 message_id_keyword = ["message_id", "msgId", "id"]
 
 
@@ -15,6 +17,9 @@ def calculate_expired_at(expire_time: timedelta | int) -> datetime:
 
 
 def get_message_id(message: Any) -> str | None:
+    if isinstance(message, Receipt):
+        message = message.msg_ids[0]
+
     return next(
         (message.get(key) for key in message_id_keyword if key in message),
     )
